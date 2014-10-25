895e28d811aa7eb2637fb4a59d87f85173a451e0
zumoshi <bor@ns-apps.tk>
2014-10-25 18:49:14 +0330

    added test cases (node/vows)

---
+++ test/jdate.js
@@ -0,0 +1,59 @@
+// division-by-zero-test.js
+
+var vows = require('vows'),
+    assert = require('assert');
+var JDate  = require('./../tarikh.js')
+
+// Create a Test Suite
+vows.describe('Convert date to shamsi').addBatch({
+    'when given an string': {
+        topic: function () { return JDate("2010/5/5") },
+
+        'we get a Date Object': function (topic) {
+            assert.equal (Object.prototype.toString.call(topic.d), "[object Date]");
+        },
+        'toString': function(topic){
+            assert.equal (topic.toString(), "1389/2/15");
+        },
+        'getDate': function(topic){
+            assert.equal (topic.getDate(), 15);
+        },
+        'getMonth': function(topic){
+            assert.equal (topic.getMonth(), 2);
+        },
+        'getYear': function(topic){
+            assert.equal (topic.getYear(), 1389);
+        },
+        'getMah': function(topic){
+            assert.equal (topic.getMah(), "اردیبهشت");
+        },
+        'getRooz': function(topic){
+            assert.equal (topic.getRooz(), 'چهار شنبه');
+        }
+    },
+    'given timestamp': {
+        topic: function () { return JDate(1273017600*1000) },
+
+        'we get a Date Object': function (topic) {
+            assert.equal (Object.prototype.toString.call(topic.d), "[object Date]");
+        },
+        'toString': function(topic){
+            assert.equal (topic.toString(), "1389/2/15");
+        },
+        'getDate': function(topic){
+            assert.equal (topic.getDate(), 15);
+        },
+        'getMonth': function(topic){
+            assert.equal (topic.getMonth(), 2);
+        },
+        'getYear': function(topic){
+            assert.equal (topic.getYear(), 1389);
+        },
+        'getMah': function(topic){
+            assert.equal (topic.getMah(), "اردیبهشت");
+        },
+        'getRooz': function(topic){
+            assert.equal (topic.getRooz(), 'چهار شنبه');
+        }
+    }
+}).export(module);
